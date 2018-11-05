38bb4086897d144530fa35b3f11452b6af8ec7d1
hikwang <hikwang@foxmail.com>
2018-11-05 11:33:57 +0800

    fix push2github.sh

--- push2github.sh
+++ push2github.sh
@@ -27,9 +27,9 @@ function handleGit(){
     fi
 
     if [ $? -eq 0 ];then
-        echo "commitMsg(auto push github):"
+        echo "commitMsg('auto push github'):"
         read commitMsg
-        git commit -m ${commitMsg:='auto push github'}
+        git commit -m "${commitMsg:='auto push github'}"
     fi
 
     if [ $? -eq 0 ];then
