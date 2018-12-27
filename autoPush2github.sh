cd0a6305f8541118d7903928e075220dab3dab18
hikwang <hikwang@foxmail.com>
2018-12-27 20:49:15 +0800

    'auto push github'

--- autoPush2github.sh
+++ autoPush2github.sh
@@ -44,8 +44,7 @@ function handleGit(){
 
 }
 # 需要push的目录 gitDir为数组
-gitDir=("/home/hikwang/github/Hi-config-files" "/home/hikwang/github/hikwang.github.io/_posts" "/home/hikwang/github/Hi-script-tool" "/home/hikwang/github/KHSummaries" "/home/hikwang/bin" "/home/hikwang/www/FELib
-")
+gitDir=("/home/hikwang/github/Hi-config-files" "/home/hikwang/github/hikwang.github.io/_posts" "/home/hikwang/github/Hi-script-tool" "/home/hikwang/github/KHSummaries" "/home/hikwang/bin" "/home/hikwang/www/FELib" "/home/hikwang/www/Practice-Demo")
 
 # 检查git是否有需要提交的文件
 for((i=0; i<${#gitDir[*]}; i++))
