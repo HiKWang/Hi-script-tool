bd996da16052b1165bb3ea2b8105624bc8ef901d
hikwang <hikwang@foxmail.com>
2019-05-25 18:35:40 +0800

    auto push github

--- CPatch2X.sh
+++ CPatch2X.sh
@@ -1,32 +1,54 @@
 #! /bin/bash
 # 创建补丁并上传到目标主机
 
-echo 'COMMIT_ID'
-read commitId
+echo '请输入起始COMMIT_ID'
+read beginCommitId
+
+echo '请输入结束COMMIT_ID(默认为HEAD)'
+read endId
+endCommitId=${endId:='HEAD'}
+
+echo 'PATCH_NAME(默认为patch)'
+read name
+patchName=${name:='patch'}
 
 echo 'HOST_IP'
 read hostIp
 
 
-createTime=`date "+%F-%H-%M"`
-patchPath='/home/hikwang/Downloads/tmp/wifiphp-patch/'
-fileName='wifiphp_'$createTime'.zip'
+createTime=`date "+%F-%H%M"`
+patchPath='/home/hikwang/Downloads/tmp/patch/'
+fileName=$patchName'_'$createTime'.zip'
 zipFile=$patchPath$fileName
 
 # 检查补丁目录是否存在
 if [ ! -d "$patchPath" ];then
     mkdir -p "$patchPath"
-else
-    git diff-tree -r --no-commit-id --name-only $commitId | xargs zip -r $zipFile
 fi
 
-if [ $? -eq 0 ];then
-    scp $zipFile 'root@'$hostIp':/home/'
+if [[ $? -eq 0 ]];then
+    #git diff-tree -r --no-commit-id --name-only $commitId | xargs zip -r $zipFile
+    git diff --name-only $beginCommitId $endCommitId | xargs zip -r $zipFile
 
-    # 上传成功删除补丁
-    #if [ $? -eq 0 ];then
-        #rm $zipFile
-    #fi
-else
-    echo "exit: $? errorMsg: 补丁打包失败";
+    if [ $? -eq 0 ];then
+        echo '补丁创建成功:'$zipFile
+
+        if [[ $hostIp -ne null ]];then
+            scp $zipFile 'root@'$hostIp':/home/'
+
+            if [ $? -eq 0 ];then
+                echo '上传成功：/home/'$fileName
+                exit 0
+            else
+                echo '上传失败！'
+                exit 1
+            fi
+        else
+            exit 0
+         fi
+    else
+        echo "补丁创建失败";
+        exit 1
+    fi
 fi
+
